9f1628d70568decda75767ce8556258a53e3d586
Emilka1604 <55575635+Emilka1604@users.noreply.github.com>
2019-11-02 19:34:39 +0300

    commit1

---
+++ samples/task2.cpp
@@ -0,0 +1,14 @@
+﻿// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+//
+// sample_prime_numbers.cpp - Copyright (c) Гергель В.П. 20.08.2000
+//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
+//
+// Тестирование битового поля и множества
+
+#include <iomanip>
+
+#include "stack.h"
+void main() {
+	ochered och;
+	och.push(3);
+}
